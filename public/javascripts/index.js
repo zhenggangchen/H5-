dedee5fb9c40e94e2ba2f61c62d55271199b566a
zhenggangchen <458409991@qq.com>
2017-04-25 15:02:31 +0800

    ajax请求服务端资源数据

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,20 +1,30 @@
 /**
  * Created by czg on 2017/4/25.
  */
-function $(s){
+function $(s) {
     return document.querySelectorAll(s);
 }
-var lis=$("#list li");
-for(var i=0;i<lis.length;i++){
-    lis[i].onclick= function () {
-        for(var j=0;j<lis.length;j++){
-            lis[j].className="";
+var lis = $("#list li");
+for (var i = 0; i < lis.length; i++) {
+    lis[i].onclick = function () {
+        for (var j = 0; j < lis.length; j++) {
+            lis[j].className = "";
         }
-        this.className="selected"
+        this.className = "selected";
+        load("/media/"+this.title);
     }
 }
 
+var xhr =new XMLHttpRequest();
 
+function load(url){
+    xhr.open("GET",url);
+    xhr.responseType="arraybuffer";
+    xhr.onload= function () {
+        console.log(xhr.response);
+    };
+    xhr.send();
+}
 
 
 
