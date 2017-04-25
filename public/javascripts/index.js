787f8d6d7a5780cac002c4689e22c7f083dd373a
zhenggangchen <458409991@qq.com>
2017-04-25 20:55:59 +0800

    实现圆点图的可视化

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -29,7 +29,7 @@ gainNode.connect(ac.destination); //所有音频输出聚集地
 
 var analyser = ac.createAnalyser();
 
-var size = 64;
+var size = 128;//控制疏密
 analyser.fftSize = size * 2; //设置FFT的大小(用于将一个信号变换到频域，默认是2048)
 analyser.connect(gainNode);
 
@@ -43,30 +43,79 @@ var canvas = document.createElement("canvas");
 box.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 
+var DOTS = [];
+function random(m, n) {
+    return Math.round(Math.random() * (n - m) + m);
+}
+function getDots() {
+    DOTS = [];
+    for (var i = 0; i < size; i++) {
+        var x = random(0, width);
+        var y = random(0, height);
+        var color = "rgb(" + random(0, 255) + "," + random(0, 255) +"," + random(0, 255) + ")";
+        DOTS.push({
+            x: x,
+            y: y,
+            color: color
+        });
+    }
+}
+var line;
 function resize() { //动态改变canva区域的宽高
     height = box.clientHeight;
     width = box.clientWidth;
     canvas.height = height;
     canvas.width = width;
-    var line = ctx.createLinearGradient(0, 0, 0, height);
+    line = ctx.createLinearGradient(0, 0, 0, height);
     line.addColorStop(0, "red");
     line.addColorStop(0.5, "yellow");
     line.addColorStop(1, "green");
-    ctx.fillStyle=line;
+
+    getDots();
 }
 resize();
 
 window.onresize = resize;
 
+
+//画柱状图
 function draw(arr) {
-    ctx.clearRect(0,0,width,height);
+    ctx.clearRect(0, 0, width, height);
     var w = width / size;
+    ctx.fillStyle = line;
     for (var i = 0; i < size; i++) {
-        var h = arr[i] / 256 * height;
-        ctx.fillRect(w * i, height - h, w * 0.6, h);
+        if (draw.type == "column") {
+            var h = arr[i] / 256 * height;
+            ctx.fillRect(w * i, height - h, w * 0.6, h); //x轴坐标,y轴坐标,宽度(0.4留为间隙),高度
+        } else if (draw.type == "dot") {
+            ctx.beginPath();
+            var o = DOTS[i];
+            var r = arr[i] / 256 * 50;
+            ctx.arc(o.x, o.y, r, 0, Math.PI * 2, true);
+            var g = ctx.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
+            g.addColorStop(0, "#fff");
+            g.addColorStop(1, o.color);
+            ctx.fillStyle = g;
+            ctx.fill();
+            //ctx.strokeStyle = "#fff";
+            //ctx.stroke();
+        }
     }
 }
 
+draw.type = "column";
+
+var types = $("#type li");
+for (var i = 0; i < types.length; i++) {
+    types[i].onclick = function () {
+        for (var j = 0; j < types.length; j++) {
+            types[j].className = "";
+        }
+        this.className = "selected";
+        draw.type = this.getAttribute("data-type");
+    }
+
+}
 
 function load(url) {
     var n = ++count;
